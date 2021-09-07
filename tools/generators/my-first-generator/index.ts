eb147c45ff983c8d0afcc2a003b486eaff07d6c7
Juri <juri.strumpflohner@gmail.com>
2021-09-07 21:08:30 +0200

    exploring some utils functions

--- tools/generators/my-first-generator/index.ts
+++ tools/generators/my-first-generator/index.ts
@@ -1,10 +1,10 @@
-import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
-import { libraryGenerator } from '@nrwl/workspace/generators';
+import { formatFiles, names, logger, Tree } from '@nrwl/devkit';
 
-export default async function (tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
-  await formatFiles(tree);
-  return () => {
-    installPackagesTask(tree);
-  };
+interface MyFirstGeneratorSchema {
+  name: string;
+}
+
+export default async function (tree: Tree, schema: MyFirstGeneratorSchema) {
+  logger.info(`Hello, ${names(schema.name).className}!`);
+  // await formatFiles(tree);
 }
