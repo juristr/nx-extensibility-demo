a889a5d8b33ea661f1a93494111328223a330739
Juri <juri.strumpflohner@gmail.com>
2021-09-07 21:20:41 +0200

    add file generation

---
+++ tools/generators/my-first-generator/files/README.md
@@ -0,0 +1,3 @@
+# Welcome
+
+Hi there, <%= conferenceName %> attendees!
--- tools/generators/my-first-generator/index.ts
+++ tools/generators/my-first-generator/index.ts
@@ -1,10 +1,37 @@
-import { formatFiles, names, logger, Tree } from '@nrwl/devkit';
+import {
+  formatFiles,
+  generateFiles,
+  names,
+  logger,
+  Tree,
+  readProjectConfiguration,
+  joinPathFragments,
+  getProjects,
+  updateProjectConfiguration,
+} from '@nrwl/devkit';
+import { libraryGenerator } from '@nrwl/workspace/generators';
 
 interface MyFirstGeneratorSchema {
   name: string;
 }
 
 export default async function (tree: Tree, schema: MyFirstGeneratorSchema) {
-  logger.info(`Hello, ${names(schema.name).className}!`);
-  // await formatFiles(tree);
+  const libName = names(schema.name).fileName;
+
+  await libraryGenerator(tree, {
+    name: libName,
+  });
+
+  // read the rootPath of the library
+  const libRootPath = readProjectConfiguration(tree, libName)?.root;
+
+  generateFiles(tree, joinPathFragments(__dirname, './files'), libRootPath, {
+    conferenceName: names(schema.name).className,
+  });
+
+  await formatFiles(tree);
+
+  // getProjects(tree).forEach((projectConfig, name) => {
+  //   logger.info(`Reading project configuration for ${name}`);
+  // });
 }
