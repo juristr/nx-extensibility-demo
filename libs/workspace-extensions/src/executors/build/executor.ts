58e988f2b39f59c2ece2a503743554a75e8eeacb
Juri <juri.strumpflohner@gmail.com>
2021-09-08 08:55:44 +0200

    create markdown builder

--- libs/workspace-extensions/src/executors/build/executor.ts
+++ libs/workspace-extensions/src/executors/build/executor.ts
@@ -1,7 +1,34 @@
+import { ExecutorContext, joinPathFragments } from '@nrwl/devkit';
 import { BuildExecutorSchema } from './schema';
+import { readFileSync, writeFileSync } from 'fs';
+import * as showdown from 'showdown';
+import { ensureDirSync } from 'fs-extra';
 
-export default async function runExecutor(options: BuildExecutorSchema) {
-  console.log('Executor ran for Build', options);
+export async function markdownToHtml(markdown) {
+  const converter = new showdown.Converter();
+  return converter.makeHtml(markdown);
+}
+
+export default async function runExecutor(
+  options: BuildExecutorSchema,
+  context: ExecutorContext
+) {
+  const projectRoot = context.workspace.projects[context.projectName].root;
+
+  // read README from project root and convert to markdown with remark
+  const readmeContent = readFileSync(
+    joinPathFragments(projectRoot, 'README.md'),
+    'utf8'
+  );
+
+  const readmeHtml = await markdownToHtml(readmeContent);
+
+  const fileOutputDir = joinPathFragments(context.root, options.outputPath);
+  ensureDirSync(fileOutputDir);
+  writeFileSync(joinPathFragments(fileOutputDir, 'README.html'), readmeHtml, {
+    encoding: 'utf8',
+  });
+
   return {
     success: true,
   };
